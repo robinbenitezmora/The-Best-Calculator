358fe244813620883acc10af3f1cd965556b1da7
robinbenitezmora <robinbenitez1990@gmail.com>
2023-12-05 19:50:20 -0500

    Create trigonometric operations

--- calculus/ScientificCalculator.java
+++ calculus/ScientificCalculator.java
@@ -43,4 +43,52 @@ public class ScientificCalculator {
   return Math.log10(a);
  }
 
+ public static double sin(double a) {
+  return Math.sin(a);
+ }
+
+ public static double cos(double a) {
+  return Math.cos(a);
+ }
+
+ public static double tan(double a) {
+  return Math.tan(a);
+ }
+
+ public static double arcsin(double a) {
+  return Math.asin(a);
+ }
+
+ public static double arccos(double a) {
+  return Math.acos(a);
+ }
+
+ public static double arctan(double a) {
+  return Math.atan(a);
+ }
+
+ public static double sinh(double a) {
+  return Math.sinh(a);
+ }
+
+ public static double cosh(double a) {
+  return Math.cosh(a);
+ }
+
+ public static double tanh(double a) {
+  return Math.tanh(a);
+ }
+
+ public static double arcsinh(double a) {
+  return Math.log(a + Math.sqrt(a * a + 1));
+ }
+
+ public static double arccosh(double a) {
+  return Math.log(a + Math.sqrt(a * a - 1));
+ }
+
+ public static double arctanh(double a) {
+  return Math.log((1 + a) / (1 - a)) / 2;
+ }
+
 }
